2648b42d41b507b31a03a909269b6d7495a4225b
zorro200 <loren2018loren@gmail.com>
2022-03-01 19:03:39 +0100

    Added explanatory comments

--- loggerMiddleware.js
+++ loggerMiddleware.js
@@ -5,8 +5,10 @@
  * @param {*} next method that will go to the next route that match with the request
  */
 const logger = (req, res, next) => {
+  // Like GET or POST
   console.log(req.method)
   console.log(req.path)
+  // Content that is handling the request's initializing
   console.log(req.body)
   console.log('----------')
   next()
