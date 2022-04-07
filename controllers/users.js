e96f817051fe7203be48bb160d94a538cbe8eb03
zorro200 <loren2018loren@gmail.com>
2022-04-07 17:34:14 +0200

    Create users controller router

---
+++ controllers/users.js
@@ -0,0 +1,22 @@
+const usersRouter = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
+// The route is relative of which we used in index.js to use this router
+usersRouter.post('/', async (req, res) => {
+  const { body } = req
+  const { nick, name, password } = body
+
+  const passwordHash = await bcrypt.hash(password, 10)
+
+  const user = new User({
+    nick,
+    name,
+    passwordHash
+  })
+
+  const savedUser = await user.save()
+  res.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
