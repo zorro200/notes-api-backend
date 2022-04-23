c6dcf5280c08a75710e2768ca9879160298cc1e8
zorro200 <loren2018loren@gmail.com>
2022-04-23 17:34:25 +0200

    Add token authorization login with jwt

--- controllers/login.js
+++ controllers/login.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/User')
@@ -12,15 +13,23 @@ loginRouter.post('/', async (req, res) => {
     ? false
     : await bcrypt.compare(password, user.passwordHash)
 
-  if (!passwordCorrect) {
+  if (!user && !passwordCorrect) {
     res.status(401).json({
       error: 'Invalid user or password'
     })
   }
 
+  // Info for token (sesion)
+  const userForToken = {
+    id: user._id,
+    nick: user.nick
+  }
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
   res.send({
     nick: user.nick,
-    name: user.name
+    name: user.name,
+    token
   })
 })
 
