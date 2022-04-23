3e5db24c5a0f6ac1a5702141f7c6422a28ace30f
zorro200 <loren2018loren@gmail.com>
2022-04-23 15:03:08 +0200

    Add get all users with their notes

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,14 @@ const User = require('../models/User')
 const bcrypt = require('bcrypt')
 
 // The route is relative of which we used in index.js to use this router
+usersRouter.get('/', async (req, res) => {
+  const users = await User.find({}).populate('notes', {
+    content: 1,
+    date: 1
+  })
+  res.json(users)
+})
+
 usersRouter.post('/', async (req, res) => {
   const { body } = req
   const { nick, name, password } = body
