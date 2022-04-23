b64e116ab3cb7097aff337c38e8c561e65a749c9
zorro200 <loren2018loren@gmail.com>
2022-04-23 20:27:14 +0200

    Refractor and update error handler

--- middleware/handleErrors.js
+++ middleware/handleErrors.js
@@ -1,9 +1,25 @@
-module.exports = (err, req, res, next) => {
-  console.error(err)
-  console.log(req.path)
-  if (err.name === 'CastError') {
-    res.status(400).send({ error: 'id used is malformed' })
-  } else {
+const ERROR_HANDLERS = {
+  CastError: res =>
+    res.status(400).send({ error: 'id used is malformed' }),
+
+  ValidationError: (res, { message }) =>
+    res.status(409).send({ error: message }),
+
+  JsonWebTokenError: res =>
+    res.status(401).json({ error: 'token missing or invalid' }),
+
+  TokenExpiredError: res =>
+    res.status(401).json({ error: 'token expired' }),
+
+  defaultError: (res, err) => {
+    console.log(err.name)
     res.status(500).end()
   }
+}
+
+module.exports = (err, req, res, next) => {
+  const handler =
+    ERROR_HANDLERS[err.name] || ERROR_HANDLERS.defaultError
+
+  handler(res, err)
 }
